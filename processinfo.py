1393e6d412547cf28b3d1fafffc196e742a7e14a
toad_lenovo <daodewang@gmail.com>
2019-09-27 16:44:41 +0800

    more analysis

--- processinfo.py
+++ processinfo.py
@@ -9,7 +9,7 @@ def getSandT():
     trains = []
     with open('Dtrain_infos.json', 'r', encoding='utf-8') as f:
         dinfo = json.load(f)
-        print(len(dinfo))
+        #print(len(dinfo))
 
         for train in dinfo:
             trains.append(train[0])
@@ -18,7 +18,7 @@ def getSandT():
 
     with open('Gtrain_infos.json', 'r', encoding='utf-8') as f:
         dinfo = json.load(f)
-        print(len(dinfo))
+        #print(len(dinfo))
 
         for train in dinfo:
             trains.append(train[0])
@@ -28,17 +28,43 @@ def getSandT():
     return stations, trains
 
 
-def getPaths():
+def sta2city(k, citys):
+    r = False
+
+    if k == '上海虹桥':
+        r = True
+        k = '上海'
+    if k == '合肥北城':
+        r = True
+        k = '合肥'
+    if k == '戚墅堰':
+        r = True
+        k = '常州'
+    if k == '深圳坪山':
+        r = True
+        k = '深圳'
+    if k in {'汉口', '武昌', '天河机场', '毛陈', '金银潭'}:
+        r = True
+        k = '武汉'
+    if k[-1] in {'东', '南', '西', '北'} and len(k) > 2:
+        r = True
+        k = k[0:-1]
+
+    return r, k
+
+
+def getTimePaths(citys):
     paths = dict()
     with open('Dtrain_infos.json', 'r', encoding='utf-8') as f:
         dinfo = json.load(f)
-        print(len(dinfo))
-        j=0
+
         for train in dinfo:
-            j=j+1
             L = len(train[1])
             for i in range(L-2):
-                edgekey = str(train[1][i][0]) + '-' + str(train[1][i+1][0])
+                r, k1 = sta2city(train[1][i][0], citys)
+                r, k2 = sta2city(train[1][i+1][0], citys)
+
+                edgekey = str(k1) + '-' + str(k2)
                 start = train[1][i][2].split(':')
                 arrive = train[1][i+1][2].split(':')
                 weight = 60*(int(arrive[0])-int(start[0]))+(int(arrive[1])-int(start[1]))
@@ -49,13 +75,14 @@ def getPaths():
 
     with open('Gtrain_infos.json', 'r', encoding='utf-8') as f:
             dinfo = json.load(f)
-            print(len(dinfo))
-            j = 0
+
             for train in dinfo:
-                j = j + 1
                 L = len(train[1])
                 for i in range(L - 2):
-                    edgekey = str(train[1][i][0]) + '-' + str(train[1][i + 1][0])
+                    r, k1 = sta2city(train[1][i][0], citys)
+                    r, k2 = sta2city(train[1][i + 1][0], citys)
+
+                    edgekey = str(k1) + '-' + str(k2)
                     start = train[1][i][2].split(':')
                     arrive = train[1][i + 1][2].split(':')
                     weight = 60 * (int(arrive[0]) - int(start[0])) + (int(arrive[1]) - int(start[1]))
@@ -64,7 +91,6 @@ def getPaths():
                     else:
                         paths[edgekey] = [weight]
 
-    print(len(paths))
     return paths
 
 
@@ -73,7 +99,7 @@ def getTimeEdges(paths):
     for k, v in paths.items():
         weight = int(sum(v)/len(v))
         nodes = k.split('-')
-        edges.append((nodes[0], nodes[1], {'weight': weight}))
+        edges.append((nodes[0], nodes[1], {'time': weight}))
     return edges
 
 
@@ -96,77 +122,89 @@ def getS_Tedge(stations):
         for train in dinfo:
             for stop in train[1]:
                 k = stop[0]
-                if k == '上海虹桥':
+                r, k1 = sta2city(k, stations)
+                if r:
                     odd.add(k)
-                    k = '上海'
-                if k == '合肥北城':
-                    odd.add(k)
-                    k = '合肥'
-                if k == '戚墅堰':
-                    odd.add(k)
-                    k = '常州'
-                if k == '深圳坪山':
-                    odd.add(k)
-                    k = '深圳'
-                if k == '汉口' or k == '武昌':
-                    odd.add(k)
-                    k = '武汉'
-                if k[-1] in {'东', '南', '西', '北'}:
-                    k1 = k[0:-1]
-                    if k1 in stations:
-                        odd.add(k)
-                        k = k1
+                    k = k1
                 edges.append((train[0], k))
 
     with open('Gtrain_infos.json', 'r', encoding='utf-8') as f:
         dinfo = json.load(f)
-        print(len(dinfo))
 
         for train in dinfo:
             for stop in train[1]:
                 k = stop[0]
-                if k == '上海虹桥':
-                    k = '上海'
-                if k == '合肥北城':
-                    k = '合肥'
-                if k == '戚墅堰':
-                    k = '常州'
-                if k == '深圳坪山':
+                r, k1 = sta2city(k, stations)
+                if r:
                     odd.add(k)
-                    k = '深圳'
-                if k == '汉口' or k == '武昌':
-                    odd.add(k)
-                    k = '武汉'
-                if k[-1] in {'东', '南', '西', '北'}:
-                    k1 = k[0:-1]
-                    if k1 in stations:
-                        odd.add(k)
-                        k = k1
+                    k = k1
                 edges.append((train[0], k))
-
-    print(len(edges))
-
     return edges, odd
 
 
 def main():
-    stations, trains = getSandT()
-    print(len(stations))
+    G = nx.DiGraph()
 
+    stations, trains = getSandT()
+    print('stations: %d' % len(stations))
+
+    # odd 是被聚类去掉的站点
+    edges, odd = getS_Tedge(stations)
+    print('odds: %d' % len(odd))
+
+    sj = stations - odd
+    print('stations - odds: %d' % len(sj))
+
+    paths = getTimePaths(sj)
+    timedges = getTimeEdges(paths)
+
+    '''
+    # 赋权图
+    G.add_nodes_from(sj)
+    G.add_edges_from(timedges)
+    
+    btw = nx.algorithms.centrality.betweenness_centrality(G, weight='time')
+    print(sorted(btw.items(), key=lambda x: (x[1]), reverse=True))
+
+    btw = nx.algorithms.centrality.betweenness_centrality(G)
+    print(sorted(btw.items(), key=lambda x: (x[1]), reverse=True))
+
+    deg = G.degree()
+    sub = [node[0] for node in deg if node[1] == 2]
+
+    addons = [ '湛江', '北海', '福州', '上海']
+    st = set(sub) | set(addons)
+    
+    btw = nx.algorithms.centrality.betweenness_centrality_subset(G,  weight='time', sources=st, targets=st)
+    print(sorted(btw.items(), key=lambda x: (x[1]), reverse=True))
+    btw = nx.algorithms.centrality.betweenness_centrality_subset(G, sources=st, targets=st)
+    print(sorted(btw.items(), key=lambda x: (x[1]), reverse=True))
+'''
+    # 二部图
     edges, odd = getS_Tedge(stations)
     print(len(odd))
 
-    sj = stations - odd
-    print(len(sj))
+    G1 = nx.DiGraph()
 
-    G = nx.DiGraph()
-    G.add_nodes_from(trains)
-    G.add_nodes_from(sj)
-    G.add_edges_from(edges)
+    G1.add_nodes_from(trains)
+    G1.add_nodes_from(sj)
+    G1.add_edges_from(edges)
 
-    ind = G.in_degree()
+    ind = G1.in_degree()
     print(ind)
     print(sorted(ind, key=lambda x: (x[1]), reverse=True))
+    data = [x[1] for x in ind if x[1] > 0]
+    print(data)
+    plt.hist(data, bins=100)
+    plt.show()
+
+    fig1, ax1 = plt.subplots()
+    ax1.hist(data, bins=1000)
+    ax1.set_xscale("log")
+    ax1.set_xlim(1e0, 1e3)
+    ax1.grid()
+    plt.draw()
+    plt.show()
 
 
 if __name__ == "__main__":
