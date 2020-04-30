b43a247fa5cca6fd537b6833b03d3eefea7a3be3
toad <daodewang@gmail.com>
2020-04-30 17:57:51 +0800

    人工处理了一些高德查不到的车站，映射关系存在oddstations.txt

---
+++ jieshu.py
@@ -0,0 +1,33 @@
+import networkx as nx
+import requests
+
+'''
+def jisuan(n, m):
+    G = nx.generators.random_graphs.gnm_random_graph(n, m)
+    betweenness = nx.algorithms.centrality.betweenness_centrality(G, endpoints=True)
+    #print(betweenness)
+    total = 0
+    for key in betweenness:
+        total += betweenness[key]
+
+    print(f'n:{n},m:{m}; total betweenness:{total}')
+
+
+for i in range(10):
+    jisuan(30, 400)
+'''
+
+
+key = '27ea3472ed190ddbc5e3160188e74111'
+address = '蓟州站'
+url = f'https://restapi.amap.com/v3/geocode/geo?address={address}&key={key}'
+
+r = requests.get(url)
+d = r.json()
+print(d)
+if d["count"] == '0':  # 未查到该车站
+    print(0)
+else:
+    city = d['geocodes'][0]['city']
+    print(city)
+
